8f72c0207737b76b0ea31561eaade9a1e1667327
AlpNov <alpnov2015@gmail.com>
2016-05-04 23:03:24 -0500

    Generated filtered and contrast stretched video
    from one video output:
    a weiner filtered video
    a contrast stretched video
    a weiner filtered then contrast stretched video
    a contrast stretched then filtered video would not be optimal since
    contrast stretching change the linearity of the image

--- main.m
+++ main.m
@@ -3,17 +3,44 @@ clear;clc;
 obj = VideoReader('ECE6364_project_data.avi');                                                                
 video=read(obj);                                                                                              
 video = video(17:512,207:805,:,:); %trim off the black border
-video_out = uint8(zeros(size(video)));
-video_out = video_out(:,:,1,:); %eliminate RGB array
+video_size = size(video);
+video_in= uint8(zeros(video_size([1 2 4])));
 num_frm=90;
+
+%gaussian blur mas identified in sample_blur.m
+mask = fspecial('gaussian',5,1.5);
+
+v_writer = VideoWriter('ECE6364_project_data_wiener.avi');
+open(v_writer);
+%apply wiener filter with the blur identified in sample_blur.m
 for k = 1:num_frm
 	Im = rgb2gray(video(:,:,:,k));
-	mask = fspecial('gaussian',5,1.5);
+	video_in(:,:,k) = Im;
 	Im_r=deconvwnr(Im, mask, 0.15);
-	video_out(:,:,:,k) = Im_r;
+	writeVideo(v_writer,Im_r);
 end
+close(v_writer);
+%discard original RGB video
+clear video;
 
-v_writer = VideoWriter('output.avi');
+
+v_writer = VideoWriter('ECE6364_project_data_contrast.avi');
 open(v_writer);
-writeVideo(v_writer,video_out);
+%apply contrast stretching
+for k = 1:num_frm
+	Im = video_in(:,:,k);
+	Im_c = histstretch(Im);
+	writeVideo(v_writer,Im_c);
+end
+close(v_writer);
+
+v_writer = VideoWriter('ECE6364_project_data_wiener_contrast.avi');
+open(v_writer);
+%apply wiener filter then contrast stretch
+for k = 1:num_frm
+	Im = video_in(:,:,k);
+	Im_r=deconvwnr(Im, mask, 0.15);
+	Im_rc = histstretch(Im_r);
+	writeVideo(v_writer,Im_rc);
+end
 close(v_writer);
